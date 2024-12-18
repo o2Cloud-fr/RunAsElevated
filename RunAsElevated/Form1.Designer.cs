da02de83627ff632be89492125a3d0e9c1a47b6c
o2Cloud <152266908+o2Cloud-fr@users.noreply.github.com>
2024-12-18 02:12:15 +0100

    v1.0

---
+++ RunAsElevated/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace RunAsElevated
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 107);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(388, 20);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 78);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Parcourir";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(319, 141);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(81, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "RunAsAdmin";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RunAsElevated.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(147, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 176);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RunAsElevated by o2Cloud";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RunAsElevated/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace RunAsElevated
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Empêcher le redimensionnement du formulaire
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            // Lire les arguments de la ligne de commande
+            string[] args = Environment.GetCommandLineArgs();
+            ProcessCommandLineArgs(args);
+        }
+        private void ProcessCommandLineArgs(string[] args)
+        {
+            // Ne rien faire si aucun argument n'est fourni
+            if (args.Length < 3) return;
+
+            try
+            {
+                string argumentType = args[1].ToLower();
+                string filePath = args[2];
+
+                // Vérifier si l'argument correspond à -r ou -rpath
+                if (argumentType == "-rpath" || argumentType == "-r")
+                {
+                    // Vérifier si le fichier existe
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        RunAsAdministrator(filePath);
+                        Environment.Exit(0);  // Force la fermeture de l'application
+                    }
+                    else
+                    {
+                        MessageBox.Show("Le fichier spécifié n'existe pas : " + filePath, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Environment.Exit(0);  // Force la fermeture de l'application
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Argument non reconnu : {argumentType}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Environment.Exit(0);  // Force la fermeture de l'application
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors du traitement des arguments : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);  // Force la fermeture de l'application
+            }
+        }
+
+
+
+        // Méthode appelée lors du clic sur le bouton "Parcourir"
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Executable Files (*.exe)|*.exe";
+                openFileDialog.Title = "Sélectionnez un fichier .exe";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFilePath.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void RunAsAdministrator(string filePath)
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = filePath,
+                    Verb = "runas", // Exécute en tant qu'administrateur
+                    UseShellExecute = true
+                };
+
+                Process.Start(startInfo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de l'exécution : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Méthode appelée lors du clic sur le bouton "Exécuter"
+        private void btnRun_Click_1(object sender, EventArgs e)
+        {
+            string filePath = txtFilePath.Text.Trim();
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Veuillez sélectionner un fichier .exe.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (System.IO.File.Exists(filePath))
+            {
+                RunAsAdministrator(filePath);
+            }
+            else
+            {
+                MessageBox.Show("Le fichier spécifié n'existe pas : " + filePath, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
